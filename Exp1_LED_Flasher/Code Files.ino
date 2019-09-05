e34032498c5e0ed360a6488317dddb3c8189c686
Ananthankichu <54417121+Ananthankichu@users.noreply.github.com>
2019-09-05 22:08:15 +0530

    Update Code Files.ino

--- Exp1_LED_Flasher/Code Files.ino
+++ Exp1_LED_Flasher/Code Files.ino
@@ -1,12 +1,12 @@
 void setup()
 {
-  pinMode(8, OUTPUT);
+  pinMode(5, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(8, HIGH);
+  digitalWrite(5, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(8, LOW);
+  digitalWrite(5, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
 }
